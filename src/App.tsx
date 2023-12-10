047290cbd7fb8e88b9192f3b96a5c02c440b8abb
Jinyoung Sung <sjy021621@gmail.com>
2023-12-10 17:14:37 +0900

    chore: emotion css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 const App = () => {
-	return <div>App</div>
+	return (
+		<main>
+			<header className="px-6 py-2 flex justify-between">
+				<div>Home</div>
+				<div>ㅎㅇ</div>
+			</header>
+
+			<div className="flex align-center justify-center">
+				<h1>About Me</h1>
+			</div>
+		</main>
+	)
 }
 
 export default App
