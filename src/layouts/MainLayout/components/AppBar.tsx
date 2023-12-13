534d3178d17299675bde87c33a94760f56067c81
Jinyoung Sung <sjy021621@gmail.com>
2023-12-13 23:50:45 +0900

    feat: Home Button Link

--- src/layouts/MainLayout/components/AppBar.tsx
+++ src/layouts/MainLayout/components/AppBar.tsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+
 const AppBar = () => {
 	return (
 		<header className="px-6 py-2 flex justify-between">
-			<div>Home</div>
+			<Link css={{ cursor: 'pointer' }} to="">
+				Home
+			</Link>
 		</header>
 	)
 }
