f074b56cdd4543831a202638df94a9bd45320442
Jinyoung Sung <sjy021621@gmail.com>
2023-12-14 00:17:12 +0900

    feat: PostPage kickoff

--- src/layouts/MainLayout/components/AppBar.tsx
+++ src/layouts/MainLayout/components/AppBar.tsx
@@ -2,10 +2,9 @@ import { Link } from 'react-router-dom'
 
 const AppBar = () => {
 	return (
-		<header className="px-6 py-2 flex justify-between">
-			<Link css={{ cursor: 'pointer' }} to="">
-				Home
-			</Link>
+		<header className="px-6 py-2 flex gap-x-4">
+			<Link to="">Home</Link>
+			<Link to="/posts">Post</Link>
 		</header>
 	)
 }
--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 
 import MainLayout from 'layouts/MainLayout'
 import HomePage from 'pages/HomePage'
+import PostPage from 'pages/PostPage'
 
 const MainRoutes = () => {
 	return (
 		<Routes>
 			<Route element={<MainLayout />}>
 				<Route path="/" element={<HomePage />} />
+				<Route path="/posts" element={<PostPage />} />
 			</Route>
 		</Routes>
 	)
