648672a5a51d0c81f11bfe70d2662a8bfd5b2a55
Jinyoung Sung <sjy021621@gmail.com>
2023-12-17 14:17:56 +0900

    feat: assert function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,7 @@
 export const isNotNil = <T>(value?: T | null | undefined): value is T => value != null
+
+export function assert(condition: boolean, ErrorMessage?: string): asserts condition {
+	if (!condition) {
+		throw new Error(ErrorMessage)
+	}
+}
